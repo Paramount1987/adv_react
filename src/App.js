edf1978825d592eb6c3491ff73fb5ac7a5a5e18c
Artem Petrov <Paramount7891@gmail.com>
2018-07-22 16:22:33 +0300

    initial config

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 
+import Root from './components/Root';
+import store  from './redux';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'react-router-redux';
+import history from './history';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello world!</h1>
-      </div>
+      <Provider store={store}>
+        <ConnectedRouter history={history}>
+          <Root/>  
+        </ConnectedRouter>
+      </Provider>
     );
   }
 }
---
+++ src/components/Root.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Root extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  };
+  }
+  render() {
+    return (
+      <div>
+        <h2>Root</h2> 
+      </div>
+    );
+  }
+}
+
+export default Root;
