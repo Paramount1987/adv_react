b03a50ad415cf871cdfc7211f8934ba401959417
Artem Petrov <Paramount7891@gmail.com>
2018-07-24 21:51:46 +0300

    add simple saga with test

--- src/ducks/people.js
+++ src/ducks/people.js
@@ -1,6 +1,7 @@
-import firebase from 'firebase';
 import {appName} from '../config';
 import { Record, List } from 'immutable';
+import { put, call, takeEvery } from 'redux-saga/effects';
+import { generateId } from './utils'; 
 
 const ReducerState = Record({
   entities: new List([])
@@ -15,6 +16,7 @@ const PersonRecord= Record({
 
 export const moduleName = 'people';
 const prefix = `${appName}/${moduleName}`;
+export const ADD_PERSON_REQUEST = `${prefix}/ADD_PERSON_REQUEST`;
 export const ADD_PERSON = `${prefix}/ADD_PERSON`;
 
 export default function reducer(state = new ReducerState(), action) {
@@ -22,7 +24,7 @@ export default function reducer(state = new ReducerState(), action) {
 
   switch (type) {
     case ADD_PERSON:
-      return state.update('entities', entities => entities.push(new PersonRecord(payload.person)));
+      return state.update('entities', entities => entities.push(new PersonRecord(payload)));
     
     default:
       return state;  
@@ -30,12 +32,31 @@ export default function reducer(state = new ReducerState(), action) {
 }
 
 export function addPerson(person) {
-  return (dispatch) => {
-    dispatch({
-      type: ADD_PERSON,
-      payload: {
-        person: {id: Date.now(), ...person}
-      }
-    });
+  return {
+    type: ADD_PERSON_REQUEST,
+    payload: person
   }
+};
+
+export const addPersonSaga = function * (action) {
+  const id = yield call(generateId);
+  yield put({
+    type: ADD_PERSON,
+    payload: {...action.payload, id}
+  });
+}
+
+// export function addPerson(person) {
+//   return (dispatch) => {
+//     dispatch({
+//       type: ADD_PERSON,
+//       payload: {
+//         person: {id: Date.now(), ...person}
+//       }
+//     });
+//   }
+// };
+
+export const saga = function * () {
+  yield takeEvery(ADD_PERSON_REQUEST, addPersonSaga);
 };
