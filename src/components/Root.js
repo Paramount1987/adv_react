1134ef832710a87d354cfd1923685a41c0a8719b
Artem Petrov <Paramount7891@gmail.com>
2018-07-30 22:58:19 +0300

    fix some

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import { Route }  from 'react-router-dom';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 import AdminPage from './routes/AdminPage';
 import AuthPage from './routes/AuthPage';
 import PersonPage from './routes/PersonPage';
 import ProtectedRoute from './common/ProtectedRoute';
+
+import { moduleName, signOut } from '../ducks/auth';
 
 class Root extends Component {
   constructor(props) {
@@ -12,8 +16,13 @@ class Root extends Component {
     this.state = {  };
   }
   render() {
+    const { signOut, signedIn } = this.props;
+    const btn = signedIn
+      ? <button onClick = {signOut}>Sign Out</button>
+      : <Link to="/auth/signin">Sign In</Link>
     return (
       <div>
+        {btn}
         <ProtectedRoute path="/admin"  component={AdminPage} />
         <Route path="/auth"  component={AuthPage} />
         <Route path="/person"  component={PersonPage} />
@@ -22,4 +31,6 @@ class Root extends Component {
   }
 }
 
-export default Root;
+export default connect(state => ({
+signedIn: !!state[moduleName].user
+}), {signOut})(Root);
--- src/ducks/auth.js
+++ src/ducks/auth.js
@@ -1,7 +1,8 @@
 import firebase from 'firebase';
 import {appName} from '../config';
 import { Record } from 'immutable';
-import { all, call, cps, put,take } from 'redux-saga/effects';
+import { all, call, cps, put, take, takeEvery } from 'redux-saga/effects';
+import { push } from 'react-router-redux';
 
 const ReducerRecord = Record({
   user: null,
@@ -13,6 +14,8 @@ export const moduleName = 'auth';
 export const SIGN_UP_REQUEST = `${appName}/${moduleName}/SIGN_UP_REQUEST`;
 export const SIGN_UP_SUCCESS = `${appName}/${moduleName}/SIGN_UP_SUCCESS`;
 export const SIGN_IN_SUCCESS = `${appName}/${moduleName}/SIGN_IN_SUCCESS`;
+export const SIGN_OUT_REQUEST = `${appName}/${moduleName}/SIGN_OUT_REQUEST`;
+export const SIGN_OUT_SUCCESS = `${appName}/${moduleName}/SIGN_OUT_SUCCESS`;
 export const SIGN_UP_ERROR = `${appName}/${moduleName}/SIGN_UP_ERROR`;
 
 export default function reducer(state = new ReducerRecord(), action) {
@@ -29,7 +32,9 @@ export default function reducer(state = new ReducerRecord(), action) {
     case SIGN_UP_ERROR:
       return state
             .set('loading', false)
-            .set('error', error);        
+            .set('error', error);
+    case SIGN_OUT_SUCCESS:
+      return new ReducerRecord();                
     default:
       return state;
   }
@@ -39,6 +44,12 @@ export function signUp (email, password) {
   return {
     type: SIGN_UP_REQUEST,
     payload: {email, password}
+  }
+}
+
+export function signOut () {
+  return {
+    type: SIGN_OUT_REQUEST
   }
 }
 
@@ -87,14 +98,14 @@ export const signUpSaga = function * () {
 export const watchStatusChange = function * () {
   const auth = firebase.auth();
 
-  try {
-    yield cps([auth, auth.onAuthStateChanged]);
-  } catch (user) {
-    yield put({
-      type: SIGN_IN_SUCCESS,
-      payload: {user}
-    });
-  }
+    try {
+      yield cps([auth, auth.onAuthStateChanged]);
+    } catch (user) {
+      yield put({
+        type: SIGN_IN_SUCCESS,
+        payload: {user}
+      });
+    }
 }
 
 // firebase.auth().onAuthStateChanged(user => {
@@ -105,9 +116,25 @@ export const watchStatusChange = function * () {
 //   })
 // });
 
+export const signOutSaga = function * () {
+  const auth = firebase.auth();
+
+  try {
+    yield call([auth, auth.signOut]);
+    yield put({
+      type: SIGN_OUT_SUCCESS
+    });
+
+    yield put(push('/auth/signin'));
+  } catch (_){
+
+  }
+}
+
 export const saga = function * () {
   yield all([
     signUpSaga(),
-    watchStatusChange()
+    watchStatusChange(),
+    takeEvery(SIGN_OUT_REQUEST, signOutSaga)
   ]);
 }
